f8bae04fdf3b4e0ebe85e25e663b8c766df13564
rajivsuting <rajivsuting16@gmail.com>
2024-01-22 14:56:08 +0530

    abc

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-// app.js
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
-const connectDB = require("./db/connectDB");
 
+// Allow requests from http://127.0.0.1:5501
+const corsOptions = {
+  origin: "http://127.0.0.1:5501",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+const connectDB = require("./db/connectDB");
 const postRoutes = require("./routes/postRoutes");
 
 const port = 3000;
